6c4abcc0517a46526ac4e49a28cb8391de892b6c
Mfonido Mark <favourmark05@gmail.com>
2024-08-23 00:14:43 +0100

    fix(usetoast): removed use toats configuration

--- src/plugins/appGlobalProperties.js
+++ src/plugins/appGlobalProperties.js
@@ -5,16 +5,13 @@ import firebaseApp from '@/https/firebase' // Import your Firebase configuration
 // Initialize Firestore
 const firestore = getFirestore(firebaseApp)
 
-// Toast Property
-import { useToast } from 'primevue/usetoast';
-const toast = useToast();
+
 
 
 const setupGlobalProperties = (app) => {
   app.config.globalProperties.$firestore = firestore
   app.config.globalProperties.$getDocs = getDocs
   app.config.globalProperties.$collection = collection
-  app.config.globalProperties.$toast = toast
 }
 
 export default setupGlobalProperties
