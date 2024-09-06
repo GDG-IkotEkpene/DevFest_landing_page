4db7ce525463bec4c56001bcd8c1979c4267e84c
Slyde619 <slyde619@gmail.com>
2024-09-06 23:55:16 +0100

    feat(footer): implememting footer

--- src/components/navigation/pageFooter.vue
+++ src/components/navigation/pageFooter.vue
@@ -1,15 +1,296 @@
 <template>
   <div>
-    Footer
+    <footer class="footer">
+      <div class="container">
+        <div
+          class="row d-flex flex-column flex-md-row justify-content-evenly align-items-center gap-2 gap-md-2 gap-lg-1 p-8"
+        >
+          <div class="col col-md-5 py-3">
+            <div class="footer__card px-4 bg-card-deep">
+              <h3 class="footer__heading text-uppercase">Partner with us</h3>
+              <p class="my-4">
+                Join us as a partner to collaborate on exciting tech initiatives
+                and events. Together, we can drive innovation and create
+                valuable opportunities for the community.
+              </p>
+              <div
+                class="footer__card-bottom d-flex gap-4 align-items-start align-items-md-center"
+              >
+                <div class="bg-email">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-label="Gmail"
+                    role="img"
+                    viewBox="0 0 512 512"
+                    class="gmail"
+                  >
+                    <rect width="512" height="512" rx="15%" fill="#041329" />
+                    <path
+                      d="M158 391v-142l-82-63V361q0 30 30 30"
+                      fill="#4285f4"
+                    />
+                    <path
+                      d="M 154 248l102 77l102-77v-98l-102 77l-102-77"
+                      fill="#ea4335"
+                    />
+                    <path
+                      d="M354 391v-142l82-63V361q0 30-30 30"
+                      fill="#34a853"
+                    />
+                    <path
+                      d="M76 188l82 63v-98l-30-23c-27-21-52 0-52 26"
+                      fill="#c5221f"
+                    />
+                    <path
+                      d="M436 188l-82 63v-98l30-23c27-21 52 0 52 26"
+                      fill="#fbbc04"
+                    />
+                  </svg>
+                </div>
+                <p class="my-0 email-content">
+                  GDG Ikot Ekpene <br />
+                  <a
+                    href="mailto:gdgikotekepen@gmail.com"
+                    class="link-offset-2 link-underline link-underline-opacity-0"
+                    >GDGIkotEkpene@gmail.com</a
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col col-md-5 py-3">
+            <div class="footer__card px-4 bg-card-transparent rounded">
+              <h3 class="footer__heading text-uppercase">Connect with us</h3>
+              <p class="my-4">
+                Stay updated and engaged with GDG Ikot Ekpene by connecting with
+                us through our social media channels and contact options. We’d
+                love to hear from you and welcome you to our community.
+              </p>
+              <div class="footer__card-bottom d-flex gap-4 align-items-center">
+                <div
+                  class="footer__card-links d-flex justify-content-between gap-4"
+                >
+                  <a
+                    href="https://x.com/gdgikotekpene"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="x-link"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill="#1E45A5"
+                        d="M9.294 6.928L14.357 1h-1.2L8.762 6.147L5.25 1H1.2l5.31 7.784L1.2 15h1.2l4.642-5.436L10.751 15h4.05zM7.651 8.852l-.538-.775L2.832 1.91h1.843l3.454 4.977l.538.775l4.491 6.47h-1.843z"
+                      />
+                    </svg>
+                  </a>
+                  <a
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="medium-link"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="#1E45A5"
+                        d="M13.41 12.007c0 3.45-2.779 6.247-6.205 6.247S1 15.458 1 12.007S3.778 5.76 7.205 5.76s6.204 2.797 6.204 6.247m6.806 0c0 3.248-1.39 5.88-3.102 5.88c-1.714 0-3.103-2.633-3.103-5.88s1.39-5.88 3.103-5.88s3.102 2.633 3.102 5.88m2.784 0c0 2.91-.489 5.269-1.091 5.269c-.603 0-1.091-2.36-1.091-5.269s.488-5.269 1.091-5.269S23 9.098 23 12.008"
+                      />
+                    </svg>
+                  </a>
+                  <a
+                    href="https://chat.whatsapp.com/L45GYDXCqxVKDcB0Q5Ulgy"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="whatsapp-link"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fill="#1E45A5"
+                        d="M16.8 5.7C14.4 2 9.5.9 5.7 3.2C2 5.5.8 10.5 3.2 14.2l.2.3l-.8 3l3-.8l.3.2c1.3.7 2.7 1.1 4.1 1.1c1.5 0 3-.4 4.3-1.2c3.7-2.4 4.8-7.3 2.5-11.1m-2.1 7.7c-.4.6-.9 1-1.6 1.1c-.4 0-.9.2-2.9-.6c-1.7-.8-3.1-2.1-4.1-3.6c-.6-.7-.9-1.6-1-2.5c0-.8.3-1.5.8-2q.3-.3.6-.3H7c.2 0 .4 0 .5.4c.2.5.7 1.7.7 1.8c.1.1.1.3 0 .4c.1.2 0 .4-.1.5s-.2.3-.3.4c-.2.1-.3.3-.2.5c.4.6.9 1.2 1.4 1.7c.6.5 1.2.9 1.9 1.2c.2.1.4.1.5-.1s.6-.7.8-.9s.3-.2.5-.1l1.6.8c.2.1.4.2.5.3c.1.3.1.7-.1 1"
+                      />
+                    </svg>
+                  </a>
+                  <a href="#" target="_blank" rel="noopener noreferrer">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="#1E45A5"
+                        d="M12 2a10 10 0 1 0 0 20a10 10 0 0 0 0-20m6.235 11.43c-.072 0-.155.123-.145.196a3.58 3.58 0 0 1-1.676 3.26a6.7 6.7 0 0 1-3.025 1.184a8.2 8.2 0 0 1-4.115-.36a5.4 5.4 0 0 1-2.799-2.058a3.1 3.1 0 0 1-.514-1.996a.27.27 0 0 0-.154-.298a1.534 1.534 0 1 1 1.687-2.49a.195.195 0 0 0 .288 0a7.2 7.2 0 0 1 3.56-1.162c.4 0 .4 0 .483-.422c.226-1.029.453-2.058.669-3.086c.062-.289.185-.37.463-.31l2.253.474a.216.216 0 0 0 .257-.092a1.03 1.03 0 0 1 1.204-.433a1.1 1.1 0 0 1 .782 1.03a1.13 1.13 0 0 1-.659 1.028a1.11 1.11 0 0 1-1.45-.607c0-.113-.062-.226-.103-.35l-2.212-.463c-.226 1.03-.453 2.14-.69 3.22a7.66 7.66 0 0 1 4.054 1.297a1.52 1.52 0 0 1 1.163-.412a1.49 1.49 0 0 1 1.378 1.142a1.55 1.55 0 0 1-.69 1.708z"
+                      />
+                      <path
+                        fill="#1E45A5"
+                        d="M14.53 16.105a3.95 3.95 0 0 1-2.345.751a4.02 4.02 0 0 1-2.736-.792a.236.236 0 0 1 0-.36a.26.26 0 0 1 .303-.067a.3.3 0 0 1 .088.067a4.12 4.12 0 0 0 3.827.35q.286-.127.535-.32c.298-.298.648.103.329.37m-3.837-2.921a1.08 1.08 0 0 1-1.348 1.029h-.093a1.03 1.03 0 0 1-.7-.977a1.03 1.03 0 0 1 1.03-1.091a1.1 1.1 0 0 1 1.11 1.04m4.774-.001a1.028 1.028 0 1 1-2.05-.164a1.028 1.028 0 0 1 2.05.164"
+                      />
+                    </svg>
+                  </a>
+                  <a href="http://" target="_blank" rel="noopener noreferrer">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fill="#1E45A5"
+                        d="M17.04 17.043h-2.962v-4.64c0-1.107-.023-2.531-1.544-2.531c-1.544 0-1.78 1.204-1.78 2.449v4.722H7.793V7.5h2.844v1.3h.039c.397-.75 1.364-1.54 2.808-1.54c3.001 0 3.556 1.974 3.556 4.545zM4.447 6.194c-.954 0-1.72-.771-1.72-1.72s.767-1.72 1.72-1.72a1.72 1.72 0 0 1 0 3.44m1.484 10.85h-2.97V7.5h2.97zM18.522 0H1.476C.66 0 0 .645 0 1.44v17.12C0 19.355.66 20 1.476 20h17.042c.815 0 1.482-.644 1.482-1.44V1.44C20 .646 19.333 0 18.518 0z"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Footer sub -->
+      <div class="footer__sub">
+        <div class="container">
+          <div class="row">
+            <div
+              class="col d-flex flex-column flex-md-row justify-content-between align-contents-center"
+            >
+              <div class="item">
+                <p class="mb-sm-2 mb-0">
+                  <sup>&copy;</sup> GDGIK {{ year }}. All rights reserved
+                </p>
+              </div>
+              <div
+                class="item d-flex justify-content-between gap-3 text-capitalize"
+              >
+                <p class="mb-0">Terms of service</p>
+                <p class="mb-0">privacy</p>
+                <p class="mb-0">policy</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.footer {
+  background: hsla(223, 96%, 18%, 0.66);
+  color: hsl(0, 0%, 89%);
+  min-height: 100%;
+  overflow: hidden;
 
+  & .footer__card {
+    max-width: 520px;
+
+    & .footer__heading {
+      letter-spacing: 0.27em;
+    }
+  }
+
+  & .p-8{
+    /* min-height: 6.3em; */
+    /* padding-top: 3.4em; */
+  }
+
+  & .bg-card-deep {
+    background: hsl(227, 64%, 4%);
+    border: 2px solid transparent;
+    border-image: linear-gradient(
+      -90deg,
+      #063c8d,
+      #063c8d,
+      #11619f,
+      #063c8d,
+      #11619f
+    );
+    border-radius: 8px;
+    border-image-slice: 1;
+  }
+
+  & .bg-card-transparent {
+    border: 2px solid transparent;
+    border-image: linear-gradient(100deg, #aec9f0, #e6ebf3, #6697df) 1;
+    padding: 2.44em 0;
+  }
+
+  & .gmail {
+    width: 70px;
+    height: 70px;
+  }
+
+  & .email-content {
+    & p {
+      color: #6697df;
+      font-weight: 200 !important;
+    }
+
+    & a {
+      color: #c2cdde;
+      font-weight: 300;
+    }
+  }
+
+  & .footer__card-bottom {
+    flex-wrap: wrap;
+
+    & .footer__card-links {
+      & a svg {
+        height: 35px;
+        width: 35px;
+      }
+
+      & a {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  & .footer__sub {
+    background: #1a3d90;
+    padding: 1.5em 0;
+  }
+}
+
+@media (max-width: 576px) {
+  .p-8{
+    /* min-height: calc(100vh - 60vh); */
+  }
+    .footer__card {
+        padding: 2rem;
+    }
+
+    .footer__card-bottom {
+        flex-direction: column;
+        align-items: flex-start;
+    }
+
+    .footer__card-links {
+        flex-wrap: wrap;
+    }
+}
 </style>
